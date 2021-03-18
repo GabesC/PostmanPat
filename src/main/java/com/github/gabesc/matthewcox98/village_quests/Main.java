86b82143cfb7b2de84ecfcc99905834d82091121
Gabriel Coventry <gabrielcoventrywork@gmail.com>
2021-03-18 16:50:43 +0000

    Changed file structure to represent it as our own project instead of a tutorial

---
+++ src/main/java/com/github/gabesc/matthewcox98/village_quests/Main.java
@@ -0,0 +1,22 @@
+package com.github.gabesc.matthewcox98.village_quests;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Main implements ModInitializer {
+    public static final Item GABE_ITEM = new Item(new FabricItemSettings().maxDamage(50).group(ItemGroup.MISC));
+    public static final Item COOL_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("village_quests", "cool_item"), COOL_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("village_quests", "gabe_item"), GABE_ITEM);
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        //System.out.println("Hello Fabric world!");
+    }
+}
