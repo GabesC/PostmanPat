5f34b038dd67875ff19a802c13c9ddd773685901
Gabriel Coventry <gabrielcoventrywork@gmail.com>
2021-03-31 17:40:45 +0100

    Added postbox block along with the postbox.png for textures and started implementing an entity for the block to function as a container
    Signed-off-by: Gabriel Coventry <gabrielcoventrywork@gmail.com>

---
+++ src/main/java/com/github/gabesc/matthewcox98/village_quests/PostBoxChestEntity.java
@@ -0,0 +1,53 @@
+package com.github.gabesc.matthewcox98.village_quests;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+public class PostBoxChestEntity extends LootableContainerBlockEntity {
+   private DefaultedList<ItemStack> inventory;
+   private static final int INVENTORY_SIZE = 9;
+
+    protected PostBoxChestEntity(BlockEntityType<?> blockEntityType) {
+        super(blockEntityType);
+    }
+
+
+    @Override
+    protected DefaultedList<ItemStack> getInvStackList() {
+        return this.inventory;
+    }
+
+    @Override
+    protected void setInvStackList(DefaultedList<ItemStack> list) {
+        this.inventory = list;
+    }
+
+    @Override
+    protected Text getContainerName() {
+        return new TranslatableText("container.chest");
+    }
+
+    @Override
+    protected ScreenHandler createScreenHandler(int syncId, PlayerInventory playerInventory) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return INVENTORY_SIZE;
+    }
+}
+
